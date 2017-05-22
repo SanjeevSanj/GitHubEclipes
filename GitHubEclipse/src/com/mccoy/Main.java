83d549af77714d6f80bed420ac34fe7eeeae4ce1
Chennappa <Chennappa@DESKTOP-OR876FD>
2017-05-22 21:16:42 +0530

    Initial checking a sales tool

---
+++ GitHubEclipse/src/com/mccoy/Main.java
@@ -0,0 +1,16 @@
+package com.mccoy;
+
+public class Main {
+public static void main(String args[]){
+	SalesData data=new SalesData();
+	displayGreeting();
+	data.display();
+}
+
+private static void displayGreeting() {
+	// TODO Auto-generated method stub
+	System.out.println("hai sales happy");
+	System.out.println("hajj");
+	
+}
+}
---
+++ GitHubEclipse/src/com/mccoy/SalesData.java
@@ -0,0 +1,9 @@
+package com.mccoy;
+
+public class SalesData {
+	int data[]={0,4,42};
+	public void display(){
+		System.out.println("100 implements");
+	}
+
+}
